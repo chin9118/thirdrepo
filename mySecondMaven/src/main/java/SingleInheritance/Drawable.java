898d85c4d08e0bd9d34a5a8f79af523ea9bf07cb
Jishnu <Jishnu@LAPTOP-VBIL4K2H>
2023-06-12 02:07:32 +1000

    commit interface

---
+++ mySecondMaven/src/main/java/SingleInheritance/Drawable.java
@@ -0,0 +1,15 @@
+package SingleInheritance;
+
+public class Drawable implements Printable{
+	public void show()
+	{
+		System.out.println("hai");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Drawable d=new Drawable();
+d.show ();
+	}
+
+}
---
+++ mySecondMaven/src/main/java/SingleInheritance/Printable.java
@@ -0,0 +1,6 @@
+package SingleInheritance;
+
+public interface Printable {
+void show();
+
+}

870cc4d40c4195e0d9d492d4e824411aeb70716e
Jishnu <Jishnu@LAPTOP-VBIL4K2H>
2023-06-13 19:30:51 +1000

    setter and getter commit

---
+++ mySecondMaven/src/main/java/SingleInheritance/Student1.java
@@ -0,0 +1,12 @@
+package SingleInheritance;
+
+public class Student1 {
+	private int age=25;
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+}
---
+++ mySecondMaven/src/main/java/SingleInheritance/Student2.java
@@ -0,0 +1,9 @@
+package SingleInheritance;
+
+public class Student2 {
+public static void main(String args[])
+{
+	Student1 obj=new Student1();
+	System.out.println("Age is"+obj.getAge());
+}
+}

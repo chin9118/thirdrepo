e40b13289ddc283c45d83c226e8679bea8850ff1
Jishnu <Jishnu@LAPTOP-VBIL4K2H>
2023-06-13 19:31:28 +1000

    execution successful

---
+++ mySecondMaven/src/main/java/SingleInheritance/Employee2.java
@@ -0,0 +1,14 @@
+package SingleInheritance;
+
+public class Employee2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Employee1 obj=new Employee1();
+obj.setName("Chinchu");
+obj.setSalary(1000);
+System.out.println("Name is"+obj.getName());
+System.out.println("Salary is"+obj.getSalary());
+	}
+
+}

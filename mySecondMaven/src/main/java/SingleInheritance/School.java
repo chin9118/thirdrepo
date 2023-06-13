3aabbb37d353da566e5fd198d8732e163db7aab1
Jishnu <Jishnu@LAPTOP-VBIL4K2H>
2023-06-13 19:30:08 +1000

    gettersetter commit

---
+++ mySecondMaven/src/main/java/SingleInheritance/School.java
@@ -0,0 +1,11 @@
+package SingleInheritance;
+
+public class School {
+	private String name="TDHS";
+
+	public String getName() {
+		return name;
+	}
+
+	
+}
---
+++ mySecondMaven/src/main/java/SingleInheritance/Schoolfile.java
@@ -0,0 +1,12 @@
+package SingleInheritance;
+
+public class Schoolfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		School obj=new School();
+		System.out.println(obj.getName());
+
+	}
+
+}
